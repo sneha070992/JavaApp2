34ba720fa68116444d1f5e92a09c1c39b879cdf1
sneha070992 <snehanka.dash@gmail.com>
2023-01-03 13:00:36 +0530

    another message from snehanka

--- xyz.java
+++ xyz.java
@@ -4,6 +4,7 @@ public static void main(String args[])
  int n1=0,n2=1,n3=2,i,count=10;    
  System.out.print(n1+" "+n2);//printing 0 and 1  
  System.out.print(n2+" "+n3);//printing 1 and 2
+ System.out.print(n2+" "+n3);//printing 1 and 2
 
     
  for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
